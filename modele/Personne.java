292112c6e8428e75626a8bbc6a611fdc32801b97
NsTiK9 <38789864+NsTiK9@users.noreply.github.com>
2018-06-05 22:21:58 +0200

    Update Personne.java

--- modele/Personne.java
+++ modele/Personne.java
@@ -1,19 +1,36 @@
 package modele;
 
-
+/**
+ * 
+ * @author Cheffou Gang
+ *
+ */
 public class Personne {
 	private String nom;
 	private String mail;
 	
+	/**
+	 * Construit une instance de Personne
+	 * @param nom 	Le nom de la personne
+	 * @param mail 	Le mail de la personne
+	 */
 	public Personne(String nom, String mail){
 		this.nom = nom;
 		this.mail = mail;
 	}
 	
+	/**
+	 * Retourne le nom de la personne
+	 * @return le nom dela personne
+	 */
 	public String getNom(){
 		return this.nom;
 	}
 	
+	/**
+	 * Retourne le mail de la personne
+	 * @return le mail de la personne
+	 */
 	public String getMail(){
 		return this.mail;
 	}
