f804f36f13af780de489316391131837d09abcab
Thomas Blanco <38260719+Thomasbl@users.noreply.github.com>
2018-06-07 09:33:33 +0000

    Update PhotoAlreadyHereException.java

--- exception/PhotoAlreadyHereException.java
+++ exception/PhotoAlreadyHereException.java
@@ -3,14 +3,21 @@ package exception;
 public class PhotoAlreadyHereException extends Exception {
 	private String nom;
 	
+	/**
+	* Indique une photo unique
+	* @param n		nom de l'exception
+	* @param message	
+	*/
+	
 	public PhotoAlreadyHereException(String n, String message) {
 		super(message);
 		this.nom=n;
 	}
 	
 	/**
-	* Retourne l'excep
-	/
+	* Retourne l'exception sous la forme d'une chaîne de caractères.
+	*/
+	
 	public String toString() {
 		return super.toString()+" Photo déja contenue dans l'album : " + this.nom;
 	}
