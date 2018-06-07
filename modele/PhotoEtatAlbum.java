6d79ff31ea68ce22835b67d324152f467e71a925
NsTiK9 <38789864+NsTiK9@users.noreply.github.com>
2018-06-07 08:58:08 +0000

    Update PhotoEtatAlbum.java

--- modele/PhotoEtatAlbum.java
+++ modele/PhotoEtatAlbum.java
@@ -1,29 +1,56 @@
 package modele;
 
+/**
+ * Classe 
+ * @author Younes Cheffou; Haseeb Javaid; Thomas Blanco; Mathieu Jugi
+ *
+ */
 public class PhotoEtatAlbum {
-	private String monAlbum;
-	private Photo maPhoto;
-	private String etat;
+	// Variables d'instances
+	private String monAlbum;			// Le nom de l'album
+	private Photo maPhoto;				// La photo
+	private String etat;				// L'état de la photo dans l'album
 	
+	/**
+	 * Construit une instance de Photo
+	 * @param al 	Le nom de l'album
+	 * @param p 	La photo
+	 * @param status 	L'état de la photo dans l'album
+	 */
 	public PhotoEtatAlbum(String al,Photo p,String status) {
-		this.monAlbum = al;
-		this.maPhoto=p;
-		this.etat=status;
+		this.monAlbum = al;			// Initialise la variable d'instance monAlbum
+		this.maPhoto=p;				// Initialise la variable d'instance maPhoto
+		this.etat=status;			// Initialise la variable d'instance etat
 	}
 	
+	/**
+	 * Return le nom de la photo
+	 * @return le nom de la photo
+	 */
 	public String getNomPhoto() {
 		return this.maPhoto.getNom();
 	}
 	
+	/**
+	 * Return l'état de la photo dans l'album
+	 * @return l'état de la photo dans l'album
+	 */
 	public String getEtat() {
 		return this.etat;
 	}
 	
+	/**
+	 * Change l'état de la photo dans l'album
+	 * @param nom 	Le nouvel état de la photo dans l'album
+	 */
 	public void setEtat(String newState) {
 		this.etat=newState;
 	}
 	
-	
+	/**
+	 * Permet d'afficher le nom, l'état, etl'album de la photo sous la forme d'une chaîne de caractères
+	 * @return la chaîne de caractères
+	*/
 	public String toString() {
 		String s = new String(this.maPhoto.getNom()+ " dans l'album "+this.monAlbum+" à pour état"+this.etat);
 		return s;
