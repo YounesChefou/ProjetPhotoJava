95366918eb3addab6db61a39e1512a07930f3460
Thomas Blanco <38260719+Thomasbl@users.noreply.github.com>
2018-06-07 08:41:45 +0000

    Update WrongEventException.java

--- exception/WrongEventException.java
+++ exception/WrongEventException.java
@@ -4,10 +4,20 @@ import modele.*;
 public class WrongEventException extends Exception{
 	private String evenement;
 	
+	/**
+	*
+	* @param e
+	* @param message	
+	*/
+	
 	public WrongEventException(Event e, String message){
 		super(message);
 		this.evenement = e.getNomEvent();
 	}
+	
+	/**
+	* Retourne l'exception sous la forme d'une chaîne de caractères.
+	*/
 	
 	public String toString(){
 		return super.toString()+", Evenement de la photo : " + this.evenement;
