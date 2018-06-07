bd4bf5c817c983766dfecb592fdb10cdafccda39
Thomas Blanco <38260719+Thomasbl@users.noreply.github.com>
2018-06-07 08:48:19 +0000

    Update UnhandledFormatException.java

--- exception/UnhandledFormatException.java
+++ exception/UnhandledFormatException.java
@@ -4,10 +4,20 @@ package exception;
 public class UnhandledFormatException extends Exception{
 	private String nom;
 	
+	/**
+	* 
+	* @param n	 	nom de l'exception.
+	* @param message	message qui s'affiche a l'ouverture de l'exception.	 
+	*/
+	
 	public UnhandledFormatException(String n, String message) {
 		super(message);
 		this.nom=n;
 	}
+	
+	/**
+	* Retourne l'exception sous la forme d'une chaîne de caractères.
+	*/
 	
 	public String toString() {
 		return super.toString()+" : " + this.nom;
