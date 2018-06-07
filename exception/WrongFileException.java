11b5407a5bae40fbe4e844c0927019ddbb7e01e3
Thomas Blanco <38260719+Thomasbl@users.noreply.github.com>
2018-06-07 08:36:29 +0000

    Update WrongFileException.java

--- exception/WrongFileException.java
+++ exception/WrongFileException.java
@@ -4,12 +4,20 @@ package exception;
 public class WrongFileException extends Exception{
 private String nom;
 	
-	
+	/**
+	* 
+	* @param n	
+	* @param message	
+	*/
 	
 	public WrongFileException(String n, String message) {
 		super(message);
 		this.nom=n;
 	}
+	
+	/**
+	* Retourne l'exception sous la forme d'une chaîne de caractères.
+	*/
 	
 	public String toString() {
 		return super.toString()+" : " + this.nom;
