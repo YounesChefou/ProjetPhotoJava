321b3c9a67d42c9b01a4d70ff1f72f6f76641973
hasibo <39988600+hasibo@users.noreply.github.com>
2018-06-10 17:40:40 +0200

    Update AlbumControleur.java

--- modele/AlbumControleur.java
+++ modele/AlbumControleur.java
@@ -5,15 +5,24 @@ import java.io.File;
 */
 public class AlbumControleur {
 	private AlbumPhoto modele;
-	
+	/**
+ 	* @param al modèle controlé par le controleur
+ 	*/
 	public AlbumControleur(AlbumPhoto modele){
 		this.modele = modele;
 	}
-	
+	/**
+ 	* Notifie au modèle qu'il à été modifier (ajout d'une photo) en appelant ça méthode ajouterPhotoFile
+ 	* @param files fichier 
+ 	* @throws PhotoAlreadyHereException exception lancée lorsque la photo est déja dans l'album
+ 	*/
 	public void notificationAjoutModele(File[] files) throws PhotoAlreadyHereException{
 		this.modele.ajouterPhotosFile(files);
 	}
-	
+/**
+ * 	Notifie au modèle qu'il à été modifier (suppression d'une photo) en appelant ça méthode supprimerPhoto
+ * @param p Photo du modèle
+ */
 	public void notificationDelModele(Photo p) {
 		this.modele.supprimerPhoto(p);
 		}
